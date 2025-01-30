ef314a197f099961fc8f529cc1131f0185eff186
Benoit-Welsch <benoitwelsch@protonmail.com>
2025-01-30 06:36:43 +0000

    Test insert

--- app/src/lib/Main.svelte
+++ app/src/lib/Main.svelte
@@ -1,5 +1,28 @@
+<script lang="ts">
+  import Button from "@gzlab/uui/input/Button.svelte";
+  import db from "./db";
+  import { assessmentSchema } from "./db/schema";
+
+
+  
+  const insert = async () => {
+    await db
+      .insert(assessmentSchema)
+      .values({
+        selection: "BNF24001",
+        title: "Assessment" + Math.floor(Math.random() * 100),
+        lang: "FR",
+        level: "A1",
+        type: "MCQ",
+      })
+      .execute();
+    console.log("Inserted");
+  };
+</script>
+
 <div class="main">
   Main
+  <Button onClick={insert}>Insert</Button>
 </div>
 
 <style>
